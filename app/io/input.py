723afc0177c51fc54623ad1d4843f634e3248fb2
Ivan Sosniuk <i.sosniuk@ukma.edu.ua>
2026-02-22 12:56:07 +0200

    Define empty IO funcs

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+def read_text_from_console():
+    """Read text input from the console."""
+    pass
+
+
+def read_file_builtin(path):
+    """Read file contents using Python builtins."""
+    pass
+
+
+def read_file_with_pandas(path):
+    """Read file contents using pandas."""
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def print_text_to_console(text):
+    """Print text to the console."""
+    pass
+
+
+def write_file_builtin(path, content):
+    """Write content to a file using Python builtins."""
+    pass
